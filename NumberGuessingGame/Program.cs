9a9de47a7e99e724c2e71d5318e3139128707086
EnesSiracDev <esiractopaloglu.dev@gmail.com>
2024-01-22 23:41:33 +0300

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int right = 5;
+            Random r = new Random();
+            int x = r.Next(1, 10);
+            repeat:
+            if (right > 0)
+            {
+                Console.WriteLine("You have {0} rights.", right);
+                Console.WriteLine("Enter numbers between 1 and 10: ");
+                int sayi = int.Parse(Console.ReadLine());
+                if (sayi > 0 && sayi <= 10) { 
+                    if (sayi != x)
+                    {
+                        Console.WriteLine("Wrong Guess");
+                        right--;
+                        goto repeat;
+                    }
+                    else if (sayi == x)
+                    {
+                        Console.WriteLine("Right Guess");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter numbers between 1 and 10. (including 1 and 10)");
+                    goto repeat;
+                }
+            }
+            else if (right == 0)
+            {
+                Console.WriteLine("You have no rights anymore");
+            }
+        }
+    }
+}
